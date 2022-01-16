11b1945de72624a620664968032443b84ca2fc5c
itaasa <itaasarvin@gmail.com>
2022-01-16 00:38:19 -0500

    create get sun and sat

--- src/utils/date-helper.ts
+++ src/utils/date-helper.ts
@@ -1,38 +1,37 @@
 async function main() : Promise<void> {
-    console.log(getSundayOf(new Date()));
+    console.log(getSaturdayOfDate(new Date()));
+    console.log(getSundayOfDate(new Date()));
 }
 
 main();
 
-function getSundayOf(date: Date): Date {
-    let dateAtMidnight = getDateAtMidnight(date);
-    console.log(dateAtMidnight.getDay())
-    return dateAtMidnight;
-}
-
-function endOfWeek(date: Date): Date {
-    return date;
-}
-
-function getDateAtMidnight(date: Date): Date {
-    let dateAtMidnight = setTimeToZero(date);
-    return getDateinEST(dateAtMidnight);
-}
-
-function getDateinEST(date: Date): Date {
-    const estOffset = -5.0;
-    return new Date(date.getTime() + (3600000 * estOffset));
-}
-
-function setTimeToZero(date: Date): Date {
-    date.setHours(0,0,0,0);
-    return date;
-}
-
-function isBetweenMonth(entryDate: Date): boolean {
-    let entryDateTime = new Date(entryDate).getTime();
+function getSundayOfDate(date: Date): Date {
+    let dayOfTheWeek = date.getDay();
     
-    return entryDateTime >= new Date('01/01/2022').getTime()
-    && entryDateTime <= new Date('01/31/2022').getTime();
+    date.setDate(date.getDate() - dayOfTheWeek);
+
+    date.setHours(0,0,0,0);
+
+    return date;
+}
+
+function getSaturdayOfDate(date: Date): Date {
+    let dayOfTheWeek = date.getDay();
+
+    let offsetDays = 6 - (dayOfTheWeek % 7);
+
+    date.setDate(date.getDate() + offsetDays);
+
+    date.setHours(23, 59, 59, 999);
+
+    return date;
+}
+
+function getSundayOfTheMonth(date: Date): Date {
+
+}
+
+function getFirstDayOfMonth(date: Date): Date {
+    
 }
 
