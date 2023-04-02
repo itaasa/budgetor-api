56357fd01e5ea58f0fb1ccb376914badb8d7aa75
itaasa <itaasarvin@gmail.com>
2023-04-01 20:57:36 -0400

    added correct db name fo ruser

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,46 +1,44 @@
-import { ObjectId } from "mongodb";
-import { DbConnection } from "../db";
-import { BudgetType } from "./budget-type";
-import { TypeMax } from "./type-max";
+import { ObjectId } from 'mongodb';
+import { DbConnection } from '../db';
+import { BudgetType } from './budget-type';
+import { TypeMax } from './type-max';
 
 export interface User {
-    name: string,
-    typeMaxes: TypeMax[],
-};
-
-const userCollectionName = 'users';
-
-export async function getUser() : Promise <User> {
-    try {
-        let db = await new DbConnection().get();
-        let result = await db.collection(userCollectionName).findOne() as User;
-
-        return result;
-    } catch (e) {
-        throw (e);
-    }
+  name: string;
+  typeMaxes: TypeMax[];
 }
 
-export async function getUserById(userId: string) : Promise<User> {
-    try {
-        let db = await new DbConnection().get();
-        let result = await db.collection(userCollectionName).findOne(
-            {
-                _id: new ObjectId(userId),
-            },
-        ) as User;
+const userCollectionName = 'user';
 
-        return result;
-    } catch (e) {
-        throw (e);
-    }
-};
+export async function getUser(): Promise<User> {
+  try {
+    let db = await new DbConnection().get();
+    let result = (await db.collection(userCollectionName).findOne()) as User;
 
-export async function createUser(user: User) : Promise<string> {
-    try {
-        let db = await new DbConnection().get();
-        return await db.collection(userCollectionName).insertOne(user);
-    } catch (e) {
-        throw(e);
-    }
+    return result;
+  } catch (e) {
+    throw e;
+  }
+}
+
+export async function getUserById(userId: string): Promise<User> {
+  try {
+    let db = await new DbConnection().get();
+    let result = (await db.collection(userCollectionName).findOne({
+      _id: new ObjectId(userId),
+    })) as User;
+
+    return result;
+  } catch (e) {
+    throw e;
+  }
+}
+
+export async function createUser(user: User): Promise<string> {
+  try {
+    let db = await new DbConnection().get();
+    return await db.collection(userCollectionName).insertOne(user);
+  } catch (e) {
+    throw e;
+  }
 }
